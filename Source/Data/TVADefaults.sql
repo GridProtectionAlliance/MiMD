0933157724d2bc284aa8b280e030ce56b2cf5020
Christoph Lackner <clackner@gridprotectionalliance.org>
2021-03-02 03:30:07 -0500

    Added TVA Diagnostic and config Change Email to Defaults

--- Source/Data/TVADefaults.sql
+++ Source/Data/TVADefaults.sql
@@ -94,7 +94,7 @@ INSERT INTO ComplianceState (Description, Color, TextColor, Priority) VALUES
 ('In Compliance','#28A745','#ffffff',0),
 ('Compliance Issue','#DC3545','#ffffff',6),
 ('Acknowledged','#FFC107','#212529',5),
-('Reviewed','#2A2484','#212529',4),
+('Reviewed','#2A2484','#ffffff',4),
 ('Inactive','#f8f9fa','#212529',7),
 ('RAP Submitted','#17A2B8','#ffffff',3)
 GO
