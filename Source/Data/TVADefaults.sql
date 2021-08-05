d88bff848a0a17f5d970cc0fccfed71d759c9ad6
seanaroche <seanaroche7@gmail.com>
2021-08-05 14:25:11 -0400

    Update TVADefaults.sql

--- Source/Data/TVADefaults.sql
+++ Source/Data/TVADefaults.sql
@@ -1,96 +1,5 @@
-﻿USE [MiMD]
-GO
-
-DROP TABLE ValueList
-GO
-
-CREATE VIEW ValueList AS SELECT * FROM SystemCenter.dbo.ValueList
-GO
-
-DROP TABLE ValueListGroup
-GO
-
-CREATE VIEW ValueListGroup AS SELECT * FROM SystemCenter.dbo.ValueListGroup
-GO
-
-DROP TABLE Note
-GO
-
-CREATE VIEW Note AS
-SELECT 
-	ID,
-	ReferenceTableID as MeterID,
-	Note,
-	UserAccount,
-	Timestamp
-FROM
-	OpenXDA.dbo.Note
-WHERE NoteTypeID = (SELECT ID FROM OpenXDA.dbo.NoteType WHERE Name = 'Meter')
-GO
-
-DROP TABLE Meter
-GO
-
-CREATE VIEW Meter AS SELECT Meter.*, ValueList.Text as TSC FROM
-	OpenXDA.dbo.Meter LEFT JOIN
-	AdditionalFieldValue on Meter.ID = AdditionalFieldValue.ParentTableID AND AdditionalFieldValue.AdditionalFieldID = (SELECT ID FROM AdditionalField WHERE FieldName = 'TSC' AND ParentTable = 'Meter') LEFT JOIN
-	ValueList ON ValueList.Value = AdditionalFieldValue.Value AND ValueList.GroupID = (SELECT ID FROM ValueListGroup WHERE Name = 'TSC')
-GO
-
-
-CREATE TRIGGER Tr_Insert_Note ON Note INSTEAD OF INSERT AS
-BEGIN
-	INSERT INTO OpenXDA.dbo.Note(NoteTypeID, ReferenceTableID, Note, UserAccount, Timestamp)
-	SELECT
-		(SELECT ID FROM OpenXDA.dbo.NoteType WHERE Name = 'Meter') as NoteTypeID,
-		MeterID as ReferenceTableID,
-		Note,
-		UserAccount,
-		TimeStamp
-	FROM
-		inserted
-END
-GO
-
-CREATE TRIGGER Tr_Delete_Note ON Note INSTEAD OF DELETE AS
-BEGIN
-	DELETE FROM OpenXDA.dbo.Note WHERE ID IN (SELECT ID FROM deleted)
-END
-GO
-
-
-DROP TABLE AdditionalFieldValue
-GO
-
-CREATE VIEW AdditionalFieldValue AS 
-SELECT 
-	ID,
-	OpenXDAParentTableID as ParentTableID,
-	AdditionalFieldID,
-	Value,
-	UpdatedOn
-FROM 
-	SystemCenter.dbo.AdditionalFieldValue
-GO
-
-DROP TABLE AdditionalField
-GO
-
-CREATE VIEW AdditionalField AS 
-SELECT 
-	ID,
-	OpenXDAParentTable as ParentTable,
-	FieldName,
-	Type,
-	ExternalDB,
-	ExternalDBTable,
-	ExternalDBTableKey,
-	IsSecure
-FROM 
-	SystemCenter.dbo.AdditionalField
-GO
-
-INSERT INTO ComplianceState (Description, Color, TextColor, Priority) VALUES
+﻿
+INSERT INTO [MiMD.ComplianceState] (Description, Color, TextColor, Priority) VALUES
 ('In Compliance','#28A745','#ffffff',0),
 ('Compliance Issue','#DC3545','#ffffff',6),
 ('Acknowledged','#FFC107','#212529',5),
@@ -99,7 +8,7 @@ INSERT INTO ComplianceState (Description, Color, TextColor, Priority) VALUES
 ('RAP Submitted','#17A2B8','#ffffff',3)
 GO
 
-INSERT INTO SummaryEmail (Template, dataSQL,Subject) VALUES
+INSERT INTO [MiMD.SummaryEmail] (Template, dataSQL,Subject) VALUES
 ('<?xml version="1.0"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 <xsl:output method="xml" />
@@ -166,7 +75,7 @@ INSERT INTO SummaryEmail (Template, dataSQL,Subject) VALUES
 	'Configuration File Changes')
 GO
 
-INSERT INTO SummaryEmail (Template, dataSQL,Subject) VALUES
+INSERT INTO [MiMD.SummaryEmail] (Template, dataSQL,Subject) VALUES
 ('<?xml version="1.0"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 <xsl:output method="xml" />
@@ -240,7 +149,7 @@ INSERT INTO SummaryEmail (Template, dataSQL,Subject) VALUES
 	'Diagnostic File Alarms')
 GO
 
-INSERT INTO SummaryEmail (Template, dataSQL,Subject) VALUES
+INSERT INTO [MiMD.SummaryEmail] (Template, dataSQL,Subject) VALUES
 ('<?xml version="1.0"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 <xsl:output method="xml" />
@@ -338,7 +247,7 @@ SELECT (SELECT (
 GO
 
 -- CleanUp Tasks
-INSERT INTO DBCleanUpTask (SQLCommand, Schedule) VALUES
+INSERT INTO [MiMD.DBCleanUpTask] (SQLCommand, Schedule) VALUES
 ('DELETE FROM EMAXDiagnosticFileChanges WHERE LastWriteTime < DATEADD(DAY, -30, GETDATE())','0 3 1 * *'),
 ('DELETE FROM ConfigFileChanges WHERE LastWriteTime < DATEADD(DAY, -30, GETDATE())','10 3 1 * *')
 GO
